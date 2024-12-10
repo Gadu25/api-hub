a17646d8664eab29419495591bfcd65a0ee759e0
Gadu25 <alexanderudag@gmail.com>
2024-12-10 09:39:07 +0800

    initial setup

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<<template>
+    <div class="flex justify-center">
+        <h1 class="text-white">👋</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
