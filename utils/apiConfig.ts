bfa5217fe91b102bd33dc8802dc75a95adc4f8c1
Gadu25 <alexanderudag@gmail.com>
2024-12-16 09:37:36 +0800

    fix: test commit

--- utils/apiConfig.ts
+++ utils/apiConfig.ts
@@ -5,7 +5,7 @@ export const API_CONFIG: Record<string, string> = {
     countries: "https://restcountries.com/v3.1/",
     users: "https://randomuser.me/api/",
     joke: "https://v2.jokeapi.dev/joke/",
-    exchangeRate: "https://v6.exchangerate-api.com/v6/",
+    // exchangeRate: "https://v6.exchangerate-api.com/v4/latest/",
     // nasa: "https://api.nasa.gov/",
     // library: "https://openlibrary.org/api/",
 };
