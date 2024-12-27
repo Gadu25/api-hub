d736b32f653373270421addf836c66cb85a57fb3
Gadu25 <alexanderudag@gmail.com>
2024-12-27 16:50:37 +0800

    feat: initial design for sidebar mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,40 @@
-<script setup lang='ts'>
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const screenWidth = ref(0);
+const screenHeight = ref(0);
+
+const updateScreenSize = () => {
+  screenWidth.value = window.innerWidth;
+  screenHeight.value = window.innerHeight;
+};
+
+onMounted(() => {
+  updateScreenSize();
+  window.addEventListener('resize', updateScreenSize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateScreenSize);
+});
 </script>
 
 <template>
-
-    <div class="relative flex w-full px-6">
-        <CommonDrawerSidebar />
-        <main class="w-full">
-            <CommonNavbar />
-            <div class="content-wrapper">
-                <slot />
-            </div>
-        </main>
-    </div>
+  <div class="relative flex w-full px-6">
+    <CommonDrawerSidebar v-if="screenWidth > 768" />
+    <main class="w-full">
+      <CommonNavbar :hasMobileNav="screenWidth <= 768"/>
+      <div class="content-wrapper">
+        <slot />
+      </div>
+    </main>
+  </div>
 </template>
 
 <style scoped>
 .content-wrapper {
-    height: calc(100vh - 108px);
-    overflow-y: auto;
-    border-radius: 20px;
+  height: calc(100vh - 108px);
+  overflow-y: auto;
+  border-radius: 20px;
 }
 </style>
