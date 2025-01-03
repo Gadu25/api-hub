c2719fb804931fe49bdb2d915812a1c0666dd246
Gadu25 <alexanderudag@gmail.com>
2025-01-03 15:52:50 +0800

    adjusted, set weather as initial page

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,13 @@
 </template>
 
 <script setup>
+    const router = useRouter();
+    const redirectToWeather = async () => {
+        router.push('/weather')
+    }
 
+    onMounted(redirectToWeather);
 </script>
 
 <style lang="scss" scoped>
-
 </style>
